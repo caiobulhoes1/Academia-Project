85a2e2914c82999ac0e55c5fbd772cdda6888052
caio_ <caio_bbs@hotmail.com>
2022-08-30 21:33:09 -0300

    Inicio

---
+++ src/main/java/com/Academia/AcademiaProject/AcademiaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Academia.AcademiaProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiaProjectApplication.class, args);
+	}
+
+}
